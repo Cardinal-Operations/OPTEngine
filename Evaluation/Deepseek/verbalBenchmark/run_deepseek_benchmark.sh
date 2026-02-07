3cf020e473d14b79d7f44681b03bc07e4833f950
oneday88 <yitian1988@outlook.com>
2026-02-07 16:16:34 +0800

    rm evaluation

--- Evaluation/Deepseek/verbalBenchmark/run_deepseek_benchmark.sh
+++
@@ -1,4 +0,0 @@
-### Step Deepseek-1
-python3 s1_deepseek_generate.py --solver_name gurobi --model_name deepseek-chat --data_path ./test_data/IndustryOR_fixedV2.json
-python3 s2_deepseek_process.py --data_path ./api_return/output_s1/response_deepseek-reasoner_gurobi_MAMO_EasyLP_fixed.json
-
--- Evaluation/Qwen/qwen_eval_gurobi.sh
+++
@@ -1,5 +0,0 @@
-### The base model for new-benchmark
-python3 new_pipeline_eval.py --model_name "Qwen/Qwen3-4B-Instruct-2507"  --data_path "/home/chenyitian/PolicyDistillation/data/newPipe/knapsack" 
-python3 new_pipeline_eval.py --model_name "Qwen/Qwen3-4B-Thinking-2507"  --data_path "/home/chenyitian/PolicyDistillation/data/newPipe/knapsack" 
-
-
